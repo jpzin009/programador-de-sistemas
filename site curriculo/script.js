389a6113bb99d71dbe98f187ec6492dfedd6c99a
JOAO PEDRO BONFIM LIMA <lima.9957@aluno.pr.senac.br>
2025-06-16 16:33:57 -0300

    alterado barra de navegação

---
+++ site curriculo/script.js
@@ -0,0 +1,36 @@
+alert("Olá mundo!");
+
+//uma variavel guarda algum valor temporariamente
+//entrada dos dados
+var nome ="valdecir";
+var idade = 44;
+var senha = "123";
+
+//estrutura de condição
+if(idade > 17) {
+    alert("Pode votar! "+ nome);
+}  else{
+     alert("vai embora daqui! "+nome)
+}
+
+if(nome == "valdecir" && senha =="123"){
+    alert("login concedido!");
+}else{
+    alert("tente novamente! seu intruso!");
+}
+
+//laço de repetição
+contador =0;
+while(contador < 1000)
+{
+    contador = contador + 1;
+    console.log(contador)
+}
+
+
+var celular = document.querySelector('.celular');   //icone de menu
+var listaMenu = document.querySelector('.menu ul'); //itens do menu
+
+celular.addEventListener('click', function(){ 
+    listaMenu.classList.toggle('mostrarMenu');
+});
